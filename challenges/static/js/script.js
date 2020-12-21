5acae3aaf17be40d0e9896e6a568760bf8d2df88
Jun0922 <tjrwns126@gmail.com>
2020-12-21 13:49:39 +0900

    Challenge 3 완료

--- challenges/static/js/script.js
+++ challenges/static/js/script.js
@@ -25,3 +25,77 @@ function generateCat() {
     image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=small"
     div.appendChild(image);
 }
+
+
+// Challenge 3: 가위, 바위, 보!
+function rpsGame(yourChoice) {
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+    botChoice = numberToChoice(randToRpsInt())
+    results = decideWinner(humanChoice, botChoice);  // [0,1] human lost, bot won
+    message = finalMessage(results[0]);  //{ mesage: 'You Won', 'color': 'green'}
+    rpsFrontEnd(humanChoice, botChoice, message);
+}
+
+
+function randToRpsInt() {
+    return Math.floor(Math.random() * 3);
+}
+
+
+choices = ['rock', 'paper', 'scissors']
+function numberToChoice(number) {
+    return choices[number];
+}
+
+
+function decideWinner(yourChoice, computerChoice) {
+    var rpsDatabase = {
+        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
+        'scissors': {'paper':1, 'scissors':0.5, 'rock': 0},
+    }
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
+}
+
+
+function finalMessage(yourScore) {
+    if (yourScore === 0) {
+        return {'message': 'You lost!', 'color': 'red'};
+    }  else if (yourScore === 0.5) {
+        return {'message': 'You tied!', 'color': 'yellow'};
+    } else {
+        return {'message': 'You won!', 'color': 'blue'};
+    }
+}
+
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
+    var imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src,
+    }
+
+    // 선택권을 보여주는 사진들 지우기
+    document.getElementById('rock').remove();
+    document.getElementById('scissors').remove();
+    document.getElementById('paper').remove();
+    
+   var humanDiv = document.createElement('div');
+   var botDiv = document.createElement('div');
+   var messageDiv = document.createElement('div');
+   
+   // HTML 코드로 바로 작성 가능
+   humanDiv.innerHTML = '<img src="' + imagesDatabase[humanImageChoice] + '" height=150 width=150 style="box-shadow: 0px 10px 50px rgba(37, 50, 233, 1)">';
+   messageDiv.innerHTML = "<h1 style='color: " + message['color'] + "; font-size: 40px; padding: 30px; '>" + message['message'] +"</h1>"
+   botDiv.innerHTML = '<img src="' + imagesDatabase[botImageChoice] + '" height=150 width=150 style="box-shadow: 0px 10px 50px rgba(233, 50, 37, 1)">';
+
+   document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+   document.getElementById('flex-box-rps-div').appendChild(messageDiv);  // 여기서 같은 줄 아무리 추가해도 그만큼 추가되지 않음. Why?
+   document.getElementById('flex-box-rps-div').appendChild(botDiv);
+}
